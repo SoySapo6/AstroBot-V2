6f3203652cd5b14aca2475fce847f2604ee22018
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-05-31 14:30:56 -0500

    Deploy: Add plugins/grupo-banchat.js

---
+++ plugins/grupo-banchat.js
@@ -0,0 +1,12 @@
+let handler = async (m) => {
+
+global.db.data.chats[m.chat].isBanned = true
+conn.reply(m.chat, `《★》𝗘𝗹 𝗕𝗼𝘁 𝗛𝗮 𝗦𝗶𝗱𝗼 𝗗𝗲𝘀𝗮𝗰𝘁𝗶𝘃𝗮𝗱𝗼 𝗘𝗻 𝗘𝘀𝘁𝗲 𝗖𝗵𝗮𝘁`, m, fake)
+
+}
+handler.help = ['banchat']
+handler.tags = ['owner']
+handler.command = /^banchat$/i
+handler.prems = true 
+
+export default handler
